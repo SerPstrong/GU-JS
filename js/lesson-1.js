77193c85685da59b488d635c7480f831c65c95d1
serpstrong <ommuts@gmail.com>
2018-12-22 14:00:26 +0500

    Lesson1

---
+++ js/lesson-1.js
@@ -0,0 +1,16 @@
+var TemperatureInDegreesCelsius = +prompt("Введите значение в градусах по цельсию")
+
+var TemperatureInDegreesFahrenheit = (9 / 5) * TemperatureInDegreesCelsius + 32
+
+alert(TemperatureInDegreesFahrenheit)
+
+/*2*/
+var name = ("Василий")
+var admin = (name)
+
+alert(admin)
+/*/2*/
+
+/*3*/
+alert(1000 + "108") //Число + строка
+/*/3*/
