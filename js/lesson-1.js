2bf0aa639597013c17aa71a913377ae17cf8b9ac
serpstrong <ommuts@gmail.com>
2018-12-22 14:02:10 +0500

    Lesson1

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -5,12 +5,16 @@ var TemperatureInDegreesFahrenheit = (9 / 5) * TemperatureInDegreesCelsius + 32
 alert(TemperatureInDegreesFahrenheit)
 
 /*2*/
+
 var name = ("Василий")
 var admin = (name)
 
 alert(admin)
+
 /*/2*/
 
 /*3*/
+
 alert(1000 + "108") //Число + строка
+
 /*/3*/
